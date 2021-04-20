6fde431bc5271a9dc20768dd438858aba5335867
yishaiza <Yishai.Zaltzberg@samanage.com>
2021-04-20 10:17:06 +0300

    basic-sample

---
+++ server.js
@@ -0,0 +1,17 @@
+// require('appoptics-apm');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = 3000;
+app.listen(port);
+console.log("Running app. Visit: http://localhost:" + port + "/");
+
